66fe42ab41c586b014cc117b0832552ec05ca073
Gurleen <gurleen.singh.gsd@gmail.com>
2015-08-15 17:59:08 +0530

    Lesson3

---
+++ Lesson-3/grammarExpandExpression.py
@@ -0,0 +1,31 @@
+# Expanding Exp
+# This is very, very difficult.
+
+grammar = [ 
+    ("exp", ["exp", "+", "exp"]),
+    ("exp", ["exp", "-", "exp"]),
+    ("exp", ["(", "exp", ")"]),
+    ("exp", ["num"]),
+    ]
+
+
+def expand(tokens, grammar):
+    for pos in range(len(tokens)):
+        for rule in grammar:
+            yield rule[1]
+            
+            
+depth = 1
+utterances = [["exp"]]
+for x in range(depth):
+    for sentence in utterances:
+        utterances = utterances + [ i for i in expand(sentence, grammar)]
+
+for sentence in utterances:
+    print sentence
+    
+#    ['exp']
+#    ['exp', '+', 'exp']
+#    ['exp', '-', 'exp']
+#    ['(', 'exp', ')']
+#    ['num']
